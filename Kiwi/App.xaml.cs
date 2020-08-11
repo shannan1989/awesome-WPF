b0c9c5ec47f7c0a4a8a5b035bf15985f06a35088
山南 <zhangpeichaook@163.com>
2020-08-11 10:55:12 +0800

    #

---
+++ Kiwi/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace Kiwi
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ Shannan.Core/Utils/ProcessUtils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Shannan.Core.Utils
+{
+    public class ProcessUtils
+    {
+        public static void OpenFile(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                try
+                {
+                    Process process = new Process();
+                    process.StartInfo = new ProcessStartInfo()
+                    {
+                        FileName = fileName,
+                        Verb = "Open",
+                        CreateNoWindow = true
+                    };
+                    process.Start();
+                }
+                catch (Exception)
+                {
+                    OpenDirectory(Path.GetDirectoryName(fileName));
+                }
+            }
+            else
+            {
+                throw new Exception("打开文件失败，文件或已丢失");
+            }
+        }
+
+        public static void OpenDirectory(string directoryName)
+        {
+            Process process = new Process();
+            process.StartInfo = new ProcessStartInfo()
+            {
+                FileName = "explorer.exe",
+                Arguments = directoryName
+            };
+            process.Start();
+        }
+    }
+}
