fbb84076e9d1d2011f3d122055fd0ee7ba994ee8
山南 <zhangpeichaook@163.com>
2020-09-21 16:31:27 +0800

    #add class - ToggleButtonHelper

---
+++ Shannan.Core/Controls/ToggleButtonHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Shannan.Core.Controls
+{
+    public class ToggleButtonHelper : DependencyObject
+    {
+        public static FlowDirection GetContentDirection(DependencyObject obj)
+        {
+            return (FlowDirection)obj.GetValue(ContentDirectionProperty);
+        }
+
+        public static void SetContentDirection(DependencyObject obj, FlowDirection value)
+        {
+            obj.SetValue(ContentDirectionProperty, value);
+        }
+
+        public static readonly DependencyProperty ContentDirectionProperty = DependencyProperty.RegisterAttached("ContentDirection", typeof(FlowDirection), typeof(ToggleButtonHelper), new FrameworkPropertyMetadata(FlowDirection.LeftToRight, ContentDirectionPropertyChanged));
+
+        private static void ContentDirectionPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ToggleButton tb = d as ToggleButton;
+            if (null == tb)
+            {
+                throw new InvalidOperationException("The property 'ContentDirection' may only be set on ToggleButton elements.");
+            }
+        }
+    }
+}
