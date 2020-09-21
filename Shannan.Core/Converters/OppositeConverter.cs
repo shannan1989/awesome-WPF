b300f6605bd67883694706aa46458bacb0995c39
山南 <zhangpeichaook@163.com>
2020-09-21 11:26:27 +0800

    #add class - OppositeConverter

---
+++ Shannan.Core/Converters/OppositeConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Shannan.Core.Converters
+{
+    public sealed class OppositeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return -double.Parse(value.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return -double.Parse(value.ToString());
+        }
+    }
+}
