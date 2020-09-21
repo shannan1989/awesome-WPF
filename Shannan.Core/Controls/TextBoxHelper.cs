90d80d71c4a67fcfa7975fc37078d3dd4869ed19
山南 <zhangpeichaook@163.com>
2020-09-21 16:31:42 +0800

    #add class - TextBoxHelper

---
+++ Shannan.Core/Controls/TextBoxHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Shannan.Core.Controls
+{
+    public class TextBoxHelper : DependencyObject
+    {
+        public static string GetWatermark(DependencyObject obj)
+        {
+            return (string)obj.GetValue(WatermarkProperty);
+        }
+
+        public static void SetWatermark(DependencyObject obj, string value)
+        {
+            obj.SetValue(WatermarkProperty, value);
+        }
+
+        public static readonly DependencyProperty WatermarkProperty = DependencyProperty.RegisterAttached("Watermark", typeof(string), typeof(TextBoxHelper), new UIPropertyMetadata(string.Empty));
+    }
+}
