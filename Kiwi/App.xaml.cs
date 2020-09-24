2ed3e6942f0a421513094ac474a2d76687679958
山南 <zhangpeichaook@163.com>
2020-09-24 09:33:01 +0800

    #

--- Kiwi/App.xaml.cs
+++ Kiwi/App.xaml.cs
@@ -4,5 +4,21 @@ namespace Kiwi
 {
     public partial class App : Application
     {
+        public App()
+        {
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Current.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+        }
     }
 }
