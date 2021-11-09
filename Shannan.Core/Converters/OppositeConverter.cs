1e3b4ac0ffc09c3b1fcbf776904eff0f3826eb0e
山南 <zhangpeichaook@163.com>
2021-11-09 17:46:42 +0800

    Update OppositeConverter.cs

--- Shannan.Core/Converters/OppositeConverter.cs
+++ Shannan.Core/Converters/OppositeConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace Shannan.Core.Converters
@@ -8,11 +9,35 @@ namespace Shannan.Core.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            if (targetType == typeof(Visibility))
+            {
+                return Visibility.Visible == (Visibility)value ? Visibility.Collapsed : Visibility.Visible;
+            }
+            if (targetType == typeof(bool))
+            {
+                return !bool.Parse(value.ToString());
+            }
+            if (targetType == typeof(int))
+            {
+                return -int.Parse(value.ToString());
+            }
             return -double.Parse(value.ToString());
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            if (targetType == typeof(Visibility))
+            {
+                return Visibility.Visible == (Visibility)value ? Visibility.Collapsed : Visibility.Visible;
+            }
+            if (targetType == typeof(bool))
+            {
+                return !bool.Parse(value.ToString());
+            }
+            if (targetType == typeof(int))
+            {
+                return -int.Parse(value.ToString());
+            }
             return -double.Parse(value.ToString());
         }
     }
