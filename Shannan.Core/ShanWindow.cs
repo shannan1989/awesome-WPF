d34b880416186945cc9082ed1834a5e397fc26de
山南 <zhangpeichaook@163.com>
2021-11-10 15:58:18 +0800

    Update ShanWindow.cs

--- Shannan.Core/ShanWindow.cs
+++ Shannan.Core/ShanWindow.cs
@@ -1,4 +1,6 @@
 ﻿using MahApps.Metro.Controls;
+using Shannan.Core.Extensions;
+using System.Windows;
 
 namespace Shannan.Core
 {
@@ -6,8 +8,26 @@ namespace Shannan.Core
     {
         public ShanWindow()
         {
+            // MetroWindow
+            ShowTitleBar = false;
+            ShowCloseButton = false;
+            ShowMinButton = false;
+            ShowMaxRestoreButton = false;
+
+            IsWindowDraggable = false;
+            WindowTransitionsEnabled = false;
+
             // Window
             ShowInTaskbar = true;
+            ResizeMode = ResizeMode.CanMinimize;
+
+            Left = 0;
+            Top = 0;
+            Width = SystemParameters.WorkArea.Width;
+            Height = SystemParameters.WorkArea.Height;
+
+            Background = "#99000000".ToSolidColorBrush();
+            AllowsTransparency = true;
         }
     }
 }
