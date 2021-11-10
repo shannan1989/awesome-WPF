f5c5c7c43815f767cfc8c9f893267988918f4bb9
山南 <zhangpeichaook@163.com>
2021-11-10 15:41:00 +0800

    Create Session.cs

---
+++ Shannan.Core/Framework/Session.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Specialized;
+using System.Net;
+
+namespace Shannan.Core.Framework
+{
+    public class Session
+    {
+        private NameValueCollection _param;
+
+        static Session()
+        {
+            Instance = new Session();
+        }
+
+        public static Session Instance
+        {
+            get;
+            private set;
+        }
+
+        private Session()
+        {
+            Init();
+        }
+
+        public void Init()
+        {
+            Cookies = new CookieContainer();
+            BaseUri = string.Empty;
+            _param = new NameValueCollection();
+        }
+
+        public string BaseUri
+        {
+            get;
+            set;
+        }
+
+        public string UserAgent { get; set; } = string.Empty;
+
+        public CookieContainer Cookies
+        {
+            get;
+            private set;
+        }
+
+        public NameValueCollection Param
+        {
+            get
+            {
+                NameValueCollection param = new NameValueCollection();
+                param.Add(_param);
+
+                return param;
+            }
+            private set
+            {
+                _param = value;
+            }
+        }
+
+        public void AddPersistedParam(string name, string value)
+        {
+            _param.Add(name, value);
+        }
+
+        public void RemovePersistedParam(string name)
+        {
+            _param.Remove(name);
+        }
+
+        public void ClearPersistedParam()
+        {
+            _param.Clear();
+        }
+    }
+}
