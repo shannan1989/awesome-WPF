92601aa66482df94a23325ba973532ddc06af24f
山南 <zhangpeichaook@163.com>
2021-11-10 15:31:36 +0800

    Create JTokenExtension.cs

---
+++ Shannan.Core/Extensions/JTokenExtension.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace Shannan.Core.Extensions
+{
+    public static class JTokenExtension
+    {
+        public static List<string> Keys(this JObject self)
+        {
+            List<string> keys = new List<string>();
+            foreach (KeyValuePair<string, JToken> item in self)
+            {
+                keys.Add(item.Key);
+            }
+            return keys;
+        }
+    }
+}
