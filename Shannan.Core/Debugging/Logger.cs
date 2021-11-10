362f1982a89bf6f78d838b38a7a1d30594234456
山南 <zhangpeichaook@163.com>
2021-11-10 15:31:03 +0800

    Create Logger.cs

---
+++ Shannan.Core/Debugging/Logger.cs
@@ -0,0 +1,51 @@
+﻿using Shannan.Core.Framework;
+using SharpRaven;
+using SharpRaven.Data;
+using System;
+
+namespace Shannan.Core.Debugging
+{
+    public class Logger
+    {
+        private RavenClient _ravenClient;
+
+        static Logger()
+        {
+            Instance = new Logger();
+        }
+
+        private Logger()
+        {
+        }
+
+        public static Logger Instance
+        {
+            get;
+            private set;
+        }
+
+        public void InitRavenClient(string dsn)
+        {
+            _ravenClient = new RavenClient(dsn);
+            _ravenClient.Release = Client.Instance.AppName + "@" + Client.Instance.AppVersion;
+            _ravenClient.Environment = "production";
+#if DEBUG
+            _ravenClient.Environment = "development";
+#endif
+        }
+
+        public void ReportException(Exception e)
+        {
+            if (_ravenClient == null)
+            {
+                return;
+            }
+            try
+            {
+                _ravenClient.Capture(new SentryEvent(e));
+            }
+            catch (Exception)
+            { }
+        }
+    }
+}
