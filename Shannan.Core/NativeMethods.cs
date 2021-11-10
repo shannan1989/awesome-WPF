6b24f23492990d7ca96170d84a2b835b25a96b3e
山南 <zhangpeichaook@163.com>
2021-11-10 15:29:03 +0800

    Create NativeMethods.cs

---
+++ Shannan.Core/NativeMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace Shannan.Core
+{
+    internal class NativeMethods
+    {
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport("kernel32", CharSet = CharSet.Auto)]
+        internal static extern bool AllocConsole();
+
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport("kernel32", CharSet = CharSet.Auto)]
+        internal static extern bool FreeConsole();
+
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport("kernel32", CharSet = CharSet.Auto)]
+        internal static extern IntPtr GetConsoleWindow();
+    }
+}
