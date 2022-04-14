7a6b4e4950adab5b7567801f5c47739e3269866c
山南 <zhangpeichaook@163.com>
2022-04-14 15:32:07 +0800

    #add extension - DateTimeExtension

---
+++ Shannan.Core/Extensions/DateTimeExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shannan.Core.Extensions
+{
+    public static class DateTimeExtension
+    {
+        public static int ToUnixTimestamp(this DateTime self)
+        {
+            return (int)(self.Ticks / 10000000 - 62135596800);
+        }
+    }
+}
