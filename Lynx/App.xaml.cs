bdaa545a617d4e28ed38267d38498c3dacf58456
山南 <zhangpeichaook@163.com>
2022-04-14 16:07:29 +0800

    Lynx

---
+++ Lynx/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace Lynx
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ Lynx/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Lynx
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
