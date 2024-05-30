cbda3932ce5a34ae3cbd3381a1b56f361c24ebed
山南 <zhangpeichaook@163.com>
2024-05-30 13:47:10 +0800

    #Debugging Sentry

--- Shannan.Core/Debugging/Logger.cs
+++ Shannan.Core/Debugging/Logger.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Sentry;
+using Shannan.Core.Framework;
+using System;
 
 namespace Shannan.Core.Debugging
 {
@@ -20,12 +21,32 @@ namespace Shannan.Core.Debugging
             private set;
         }
 
-        public void ReportException(Exception e)
+        public void Init(string dsn)
         {
+            SentrySdk.Init(o =>
+            {
+                o.Dsn = dsn;
+                o.SendDefaultPii = true;
+                o.AutoSessionTracking = true;
+                o.IsGlobalModeEnabled = true; // Enabling this option is recommended for client applications only. It ensures all threads use the same global scope.
+                o.Release = Client.Instance.AppName + "@" + Client.Instance.AppVersion;
+                o.Environment = "release";
+                o.TracesSampleRate = 0.2;
+#if DEBUG
+                o.Environment = "debug";
+                o.TracesSampleRate = 1.0;
+#endif
+            });
         }
 
-        public void ReportException(Exception e, Dictionary<string, string> tags)
+        public void ReportException(Exception e)
         {
+            SentrySdk.CaptureException(e);
+        }
+
+        public void ReportMessage(string message)
+        {
+            SentrySdk.CaptureMessage(message);
         }
     }
 }
