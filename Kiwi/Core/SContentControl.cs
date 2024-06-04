2a2f87b1dba59edcc47eb88f3edda9894fbf760c
山南 <zhangpeichaook@163.com>
2024-06-04 16:55:26 +0800

    Create SContentControl.cs

---
+++ Kiwi/Core/SContentControl.cs
@@ -0,0 +1,38 @@
+﻿using Shannan.Core.Extensions;
+using System;
+
+namespace Kiwi.Core
+{
+    public class SContentControl : SBaseControl
+    {
+        public event EventHandler ShowUpCompleted;
+
+        public event EventHandler Ready;
+
+        public event EventHandler Failed;
+
+        public SContentControl()
+        {
+            Loaded += delegate
+            {
+                Opacity = 0;
+                this.BeginDoubleAnimation(OpacityProperty, 1, TimeSpan.FromSeconds(.1), delegate
+                {
+                    ShowUpCompleted?.Invoke(this, new EventArgs());
+                });
+            };
+        }
+
+        public bool NeedZoom { get; protected set; } = false;
+
+        internal void TriggerReady()
+        {
+            Ready?.Invoke(this, new EventArgs());
+        }
+
+        internal void TriggerFailed()
+        {
+            Failed?.Invoke(this, new EventArgs());
+        }
+    }
+}
